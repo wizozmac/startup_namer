59c6c971b15b52af99427b741bedd144cc17f03a
wizozmac <makoto0126@gmail.com>
2020-07-14 16:00:29 +0900

    Step 2 exercise. Don't use hot reload, if you add some new External Packages. You may get not found error, so Stop App then rewrite before run.

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:pedantic_mono/pedantic_mono.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -17,7 +19,7 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text('Hello World'),
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
